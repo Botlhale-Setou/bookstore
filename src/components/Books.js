cf4e60666746076ed4c5de4f93ac732b754d2e40
Botlhale Setou <botlhale.setou@gmail.com>
2023-01-31 20:45:53 +0200

    Complete redux-in-react functionality

--- src/components/Books.js
+++ src/components/Books.js
@@ -9,9 +9,8 @@ const Books = () => {
   return (
     <div>
       <Navbar />
-      {bookArr}
       <div className="bookList">
-        <Book title="Mastery" author="Robert Greene" />
+        {bookArr.map((book) => <Book key={book.id} title={book.title} author={book.author} />)}
       </div>
       <BookForm />
     </div>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import Navbar from './Navbar';
+import { setCompleted } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <Navbar />
-    <button type="button">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+
+  const interimCheckStatus = () => {
+    dispatch(setCompleted());
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <button type="button" onClick={interimCheckStatus}>Check status</button>
+    </div>
+  );
+};
 
 export default Categories;
